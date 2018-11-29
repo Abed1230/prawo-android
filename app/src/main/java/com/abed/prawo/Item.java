d7c10022a9e45dfddd1aa6690e6d943dcee36d12
Abed <abedfetrat@outlook.com>
2018-11-29 13:34:18 +0100

    initial commit

---
+++ app/src/main/java/com/abed/prawo/Item.java
@@ -0,0 +1,34 @@
+package com.abed.prawo;
+
+public class Item {
+    private String title;
+    private String image;
+    private String sound;
+
+    public Item() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+}
